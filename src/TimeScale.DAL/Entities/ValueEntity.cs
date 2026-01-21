545f5c2163780c633044a9218d0a593f8c16d535
SPBpiraT <intake@list.ru>
2026-01-21 23:55:36 +0300

    Added application entities

---
+++ src/TimeScale.DAL/Entities/ValueEntity.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TimeScale.DAL.Entities
+{
+    public class ValueEntity
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public double ExecutionTime { get; set; }
+
+        [Required]
+        public double Value { get; set; }
+
+    }
+}
