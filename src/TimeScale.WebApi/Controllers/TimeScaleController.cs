204b8cda19326042b98902fcc35d7fa1f9b2deff
SPBpiraT <intake@list.ru>
2026-01-21 23:37:35 +0300

    Prepare application structure

---
+++ src/TimeScale.WebApi/Controllers/TimeScaleController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TimeScale.WebApi.Models;
+
+namespace TimeScale.WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]
+    public class TimeScaleController : Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> UploadDataFromCsv(IFormFile file)
+        {
+            return StatusCode(200);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetFilteredResults([FromQuery] FilterDto filter)
+        {
+            return StatusCode(200);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetLastValues([FromQuery] string fileName)
+        {
+            return StatusCode(200);
+        }
+    }
+}
